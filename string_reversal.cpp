f01dfb180edf908f56747a52b9a7b04d9d615e4f
Ananya Swadia <ananya.swadia@gmail.com>
2025-08-17 15:43:39 +0530

    Add files via upload

---
+++ string_reversal.cpp
@@ -0,0 +1,44 @@
+//Ananya Swadia
+//24070123012
+
+#include <iostream>
+using namespace std;
+
+char* reverseString(char* str) {
+int n = 0;
+while (str[n] != '\0') {
+n++;
+}
+
+for (int i = 0; i < n / 2; i++) {
+char temp = str[i];
+str[i] = str[n - i - 1];
+str[n - i - 1] = temp;
+}
+
+return str;
+}
+
+int main() {
+const int MAX_SIZE = 100;
+char userInput[MAX_SIZE];
+
+cout << "Enter a string to reverse: ";
+cin.getline(userInput, MAX_SIZE);
+
+cout <<endl<< "Original string: " << userInput << endl;
+
+reverseString(userInput);
+
+cout << "Reversed string: " << userInput << endl;
+
+return 0;
+}
+
+/*
+Output
+Enter a string to reverse: My name is Ananya!
+
+Original string: My name is Ananya!
+Reversed string: !aynanA si eman yM
+*/
